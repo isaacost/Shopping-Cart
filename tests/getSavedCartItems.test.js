0913f99cc778985239080473392fe9a5118fcab7
Isa Costa <isa-hcosta@hotmail.com>
2022-08-09 20:31:23 -0300

    requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se ao chamar getSabedCartItems o método localStorage.getItem é chamado', () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+
+  it('Verifica se ao chamar getSabedCartItems o método localStorage.getItem é chamado com cartItems como parâmetro', () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  });
 });

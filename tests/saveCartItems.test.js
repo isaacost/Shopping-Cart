7cc5d24a28817c04c112762fc84bc11250a8a903
Isa Costa <isa-hcosta@hotmail.com>
2022-08-09 20:26:20 -0300

    requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se ao chamar saveCartItems com o argumento <ol><li>Item</li></ol> o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+
+  it('Verifica se ao chamar saveCartItems com o argumento <ol><li>Item</li></ol> o método localStorage.setItem é chamado com dois parâmetros', () => {
+    expect.assertions(1);
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });
